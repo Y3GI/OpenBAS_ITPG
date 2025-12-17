5779df0eb50abbc0f7094a26f95d26678cd1eb10
Dogru-Isim <96004353+Dogru-Isim@users.noreply.github.com>
2025-12-17 14:52:02 +0100

    Refactor agent_install.ps1 to accept hostname parameter and much more
    Added parameter for hostname and improved error handling. Updated the web request to use the provided hostname for token retrieval.

--- agent_install.ps1
+++ agent_install.ps1
@@ -1,16 +1,48 @@
+param (
+      [string]$hostname
+)
+
+$usage = "Usage: .\agent_install.ps1 openbas_url"
+
+if (-not $hostname) {
+    echo "Missing hostname"
+    echo $usage
+    exit 1
+}
+
+# Enable long file paths for the OpenBAS agent
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
+
+$openbas_base_url = "http://$hostname" + ":8080"
+
+# Add antivirus exclusions
+add-mppreference -ExclusionProcess "openbas-agent.exe"
+add-mppreference -ExclusionPath "C:\Program Files (x86)\Filigran\OBAS Agent\openbas-agent.exe"
+
+# Create the WebRequestSession object
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
 $session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0"
-$session.Cookies.Add((New-Object System.Net.Cookie("JSESSIONID", "247A28C82F6EF607ADB7077F2035AE90", "/", "openbas")))
-$res = (Invoke-WebRequest -UseBasicParsing -Uri "http://openbas:8080/api/me/tokens" `
+$session.Cookies.Add((New-Object System.Net.Cookie("JSESSIONID", "247A28C82F6EF607ADB7077F2035AE90", "/", $hostname)))
+
+# Construct the URL using the provided hostname
+$token_url = $openbas_base_url + "/api/me/tokens"
+# Make the web request to the specified URL
+$res = (Invoke-WebRequest -UseBasicParsing -Uri $token_url `
 -WebSession $session `
 -Headers @{
-"Accept"="application/json, text/plain, */*"
-  "Accept-Encoding"="gzip, deflate"
-  "Accept-Language"="en-US,en;q=0.9"
-  "Referer"="http://openbas:8080/admin/agents"
-  "responseType"="json"
+    "Accept"="application/json, text/plain, */*"
+    "Accept-Encoding"="gzip, deflate"
+    "Accept-Language"="en-US,en;q=0.9"
+    "Referer"="http://$hostname:8080/admin/agents"
+    "responseType"="json"
 }).Content | ConvertFrom-Json
 
-#echo $res
+# Output the token value
+$token = $res.token_value
+$agent_url = $openbas_base_url + "/api/agent/installer/openbas/windows/service/$token"
+iex (iwr $agent_url).Content
 
-echo $res.token_value
+# Restart for the long paths registry to take effect
+echo "Machine will restart for changes to take effect in 7 seconds"
+Start-Sleep 7
+Restart-Computer
